f1d64043de3f8db604889603a3f17d95d239e950
Pham Hoang Minh <minhrongcon2000@gmail.com>
2021-02-02 16:20:38 +0700

    Add comments

--- csv2jsonarff.py
+++ csv2jsonarff.py
@@ -3,12 +3,16 @@ from utils import csv2arffjson
 import json
 import shutil
 
+
+# remove __pycache__ folder for better performance
 shutil.rmtree("__pycache__")
 
+# generate help document
 parser = argparse.ArgumentParser(
     description="Converting csv (with any delimiter) to json so that Weka can read"
 )
 
+# arguments and documents
 parser.add_argument("-i", help="path to csv file")
 parser.add_argument("-o", help="path to output json file")
 parser.add_argument(
@@ -19,14 +23,18 @@ parser.add_argument(
     "-delim",
     help="Optional. Delimiter of csv file. Default to ,"
 )
+
+# get input from argument flags
 args = parser.parse_args()
 
+# retrieve path to csv file, output json file, configuration file, dataset's name, and delimiter
 src = args.i
 dest = args.o
 conf_path = args.conf
 relationname = args.rname if args.rname is not None else 'wekadata'
 delimiter = args.delim if args.delim is not None else ','
 
+# Exception handling with -i, -o, -conf compulsory flags
 if src is None:
     raise Exception("Source file needs providing.")
 
@@ -36,10 +44,12 @@ if dest is None:
 if conf_path is None:
     raise Exception("Attribute configuration is needed.")
 
+# load configuration file
 with open(args.conf, "r") as f:
     print("Load config file...")
     config = json.load(f)
 
+# save json file to specified path
 with open(dest, "w") as f:
     json.dump(csv2arffjson(
         src, config, relationname=relationname, delimiter=delimiter), f, indent=4)
--- utils.py
+++ utils.py
@@ -4,6 +4,14 @@ import mmap
 
 
 def get_num_lines(file_path):
+    """get the number of data point in the csv file
+
+    Args:
+        file_path (string): path to a csv file
+
+    Returns:
+        int: number of lines in csv file
+    """
     fp = open(file_path, "r+")
     buf = mmap.mmap(fp.fileno(), 0)
     lines = 0
@@ -12,17 +20,49 @@ def get_num_lines(file_path):
     return lines
 
 
+def isDataAttributeConsistent(attribute_conf, input_path, delimiter=','):
+    """check if attribute configuration has the same number of attribute as the csv dataset
+
+    Args:
+        attribute_conf (dict): attribute configuration loaded from file
+        input_path (string): path to csv file
+        delimiter (string, optional): delimiter of csv file. Defaults to ','.
+
+    Raises:
+        Exception: raised if the number of attribute in configuration is different from that number in dataset
+    """
+    with open(input_path, "r") as f:
+        for line in f:
+            if len(line.split(delimiter)) != len(attribute_conf):
+                raise Exception(
+                    'Inconsistency detected between data and attribute config')
+
+
 def getHeaderObj(relationname, attribute_conf):
+    """Generate header of ARFF file in JSON format
+
+    Args:
+        relationname (string): relation name, can be understood as name of dataset
+        attribute_conf (dict): attribute configuration load from file
+
+    Returns:
+        dict: header JSON object of ARFF format
+    """
     print("Generating header...")
 
     def getAttrObj(attr_info):
         attr_name, attr_type = attr_info
-        return {
+        attr_obj = {
             "name": attr_name,
-            "type": attr_type,
+            "type": attr_type["type"],
             "class": False,
             "weight": 1.0
         }
+
+        if attr_type['type'] == 'nominal':
+            attr_obj['labels'] = attr_type['labels']
+        return attr_obj
+
     return {
         "relation": relationname,
         "attributes": list(map(getAttrObj, attribute_conf.items()))
@@ -30,7 +70,16 @@ def getHeaderObj(relationname, attribute_conf):
 
 
 def formatData(path, delimiter=','):
-    print("format dataset...")
+    """format dataset in the form of ARFF-like JSON
+
+    Args:
+        path (string): path to csv file. Attribute names should be deleted
+        delimiter (string, optional): delimiter of csv file. Defaults to ','.
+
+    Returns:
+        list: list of JSON object representing each instance in dataset
+    """
+    print("Format dataset...")
     data = []
     with open(path, 'r') as f:
         for line in tqdm(f, total=get_num_lines(path)):
@@ -45,7 +94,19 @@ def formatData(path, delimiter=','):
 
 
 def csv2arffjson(path, attr_conf, relationname='wekadata', delimiter=','):
+    """convert csv to json that weka can read
+
+    Args:
+        path (string): path to csv file
+        attr_conf (string): attribute configuration loaded from file 
+        relationname (string, optional): name of dataset. Defaults to 'wekadata'.
+        delimiter (string, optional): delimiter of csv fil. Defaults to ','.
+
+    Returns:
+        dict: JSON format of csv file that weka can read
+    """
+    isDataAttributeConsistent(attr_conf, path, delimiter=delimiter)
     return {
         "header": getHeaderObj(relationname, attr_conf),
-        "data": formatData(path)
+        "data": formatData(path, delimiter)
     }
